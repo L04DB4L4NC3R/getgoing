0424ecc857dd04a1c5d0dce4103a92a4e4e092c0
L04DB4L4NC3R <angadsharma1016@gmail.com>
2019-05-03 13:28:36 +0530

    structs

--- section2/main.go
+++ section2/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func swap(m1, m2 *int) {
 	var temp int
 	temp = *m2
@@ -9,12 +7,12 @@ func swap(m1, m2 *int) {
 	*m1 = temp
 }
 
-func main() {
-	m1, m2 := 2, 3
-	fmt.Println(m1, m2)
-	swap(&m1, &m2)
-	fmt.Println(m1, m2)
-}
+// func main() {
+// 	m1, m2 := 2, 3
+// 	fmt.Println(m1, m2)
+// 	swap(&m1, &m2)
+// 	fmt.Println(m1, m2)
+// }
 
 // func main() {
 // 	m1 := "my name"
