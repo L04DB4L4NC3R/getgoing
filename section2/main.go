475583c1dff5f1d10d695ebf628f97c781c48ce4
L04DB4L4NC3R <angadsharma1016@gmail.com>
2019-05-03 13:07:38 +0530

    int, str and ptr

---
+++ section2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func swap(m1, m2 *int) {
+	var temp int
+	temp = *m2
+	*m2 = *m1
+	*m1 = temp
+}
+
+func main() {
+	m1, m2 := 2, 3
+	fmt.Println(m1, m2)
+	swap(&m1, &m2)
+	fmt.Println(m1, m2)
+}
+
+// func main() {
+// 	m1 := "my name"
+// 	m2 := "name"
+// 	fmt.Println(strings.Split(m1, " "), m1+m2)
+// }
+
+// func main() {
+
+// 	m1 := 2
+// 	m2 := 3
+// 	fmt.Println(m1 + m2)
+// }
