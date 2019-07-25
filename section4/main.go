5bc09943eacc1ce0fd477faa77419f246faa9dd1
L04DB4L4NC3R <angadsharma1016@gmail.com>
2019-07-25 16:25:29 +0530

    server init

---
+++ section4/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/getgoing/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("request received")
+		fmt.Println(r.Method)
+		w.Write([]byte("Hello world"))
+	})
+	mux.HandleFunc("/getgoing/go", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("request received")
+		fmt.Println(r.Method)
+		w.Write([]byte("Hello world go"))
+	})
+	http.ListenAndServe("localhost:3000", mux)
+	http.ListenAndServe("localhost:3000", mux)
+}
